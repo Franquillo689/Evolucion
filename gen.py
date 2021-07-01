772f302251aa275e42fd6fac37c051d6d90d442c
Franco Mercado <francomercado124@gmail.com>
2021-07-01 12:25:25 -0300

    Funcion return_random_char()

--- gen.py
+++ gen.py
@@ -1,3 +1,15 @@
+from random import randint
+
+def return_random_char():
+	# Return a random character between:
+	# Space ! " # $ % & ' ( ) * + ´ - . 
+	# / 0 1 2 3 4 5 6 7 8 9 : ; < = > ?
+	# @ A B C D E F G H I J K L M N O P
+	# Q R S T U V W X Y Z [ \ ] ^ _ ` a
+	# b c d e f g h i j k l m n o p q r
+	# s t u v w x y z { | } ~
+	return chr(randint(32, 126))
+
 class DNA:
 	def __init__(self, n_indiviudals, target, mutation_rate, verbose):
 		self.n_indiviudals = n_indiviudals
@@ -6,8 +18,10 @@ class DNA:
 		self.verbose       = verbose
 	
 	def create_population(self):
-		self.population = ["" for i in range(self.n_indiviudals)]
-		print(self.population)
+		# Create each dna pupulation
+		self.population = [len(self.target) * " " for i in range(self.n_indiviudals)]
+		
+		
 
 
 
@@ -18,9 +32,14 @@ def main():
 	target = "To be or not to be." # AI target
 	mutation_rate = 0.01 # Mutation rate
 	popmax = 400 # Max problation
-	model = DNA(n_indiviudals = popmax, target = target, mutation_rate = mutation_rate, verbose = False)
+	# verbose is state when AI begins, when AI gets target verbose = True
+	model = DNA(
+					n_indiviudals = popmax, 
+					target        = target, 
+					mutation_rate = mutation_rate, 
+					verbose       = False
+				  )
 	model.create_population()
-
 
 if __name__ == '__main__':
 	main()
