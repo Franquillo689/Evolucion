8a32611f398c618867f3917a1d185b8ba9af7bf6
Franco Mercado <francomercado124@gmail.com>
2021-07-11 15:28:45 -0300

    Final version 0.3

--- gen.py
+++ gen.py
@@ -11,20 +11,6 @@ class DNA:
 		self.fitness       = np.array([])  # Fitness list of population
 		self.population    = []            # DNA list of each individual
 		self.verbose       = False		     # When True => program must finish
-		self.genetic_pool  = np.array([ 
-							     ' ', '!', '#', '$', '%', '&', '(', ')', 
-							     '*', '+', ',', '-', '.', '/', '~', '|',
-	        				     '0', '1', '2', '3', '4', '5', '6', '7', 
-	       					  '8', '9', ':', ';', '<', '=', '>', '?', 
-	       					  '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 
-	       					  'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 
-	       					  'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 
-	       					  'X', 'Y', 'Z', '[', ']', '^', '_', 'a', 
-	       					  'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
-	       					  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 
-	       					  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 
-	        				     'z', '{', '}'
-	       					 ])
 
 	def create_population(self):
 		# Create each dna pupulation
